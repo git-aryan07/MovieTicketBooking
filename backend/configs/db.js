01e5091f2c5271ce145965c6ed643dca92870413
git-aryan07 <aryanchaurasiya987@gmail.com>
2025-06-28 19:38:03 +0530

    backed intialize with exrpress and db connect

---
+++ backend/configs/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+
+const connectdb= async ()=>{
+
+
+    try{
+        mongoose.connection.on('connected',()=>{
+                console.log("database connected");
+        })
+        await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`)
+    }
+    catch(error)
+    {
+        console.log(error.message);
+    }
+}
+
+export default connectdb;
